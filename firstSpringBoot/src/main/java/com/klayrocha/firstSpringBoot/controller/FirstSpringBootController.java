41cb5ba4b581869ef85be1d16702bffed19325f9
Unknown <cyborgmg@gmail.com>
2018-06-28 09:02:11 -0300

    28-06-2018

---
+++ firstSpringBoot/src/main/java/com/klayrocha/firstSpringBoot/controller/FirstSpringBootController.java
@@ -0,0 +1,19 @@
+package com.klayrocha.firstSpringBoot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstSpringBootController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index page";
+	}
+	
+	@RequestMapping("/showtext")
+	public String showText() {
+		return "Hello, Frist Spring Boot project";
+	}
+
+}
